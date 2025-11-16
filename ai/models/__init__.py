7b33982b7d572062d78616394b61e30576cf444e
Biscuit Nick <nick@biscuit.land>
2025-11-16 01:47:35 -0600

    clean branch

--- ai/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""AI processing models and schemas"""
-
-from .brand_config import *
-from .brand_style_vector import *
-from .prompt_analysis import *
-from .scene_decomposition import *
-from .brand_harmony import *
-from .replicate_client import *
-from .recomposition import *
--- frontend/src/components/History/index.ts
+++
@@ -1,4 +0,0 @@
-export { GenerationCard } from './GenerationCard';
-export { FilterSidebar } from './FilterSidebar';
-export type { GenerationCardProps } from './GenerationCard';
-export type { FilterSidebarProps } from './FilterSidebar';
